73a688c5eb59fab08dce68b86286276a6d050504
MateusMaiaDS <mmateusmaiads@gmail.com>
2025-01-15 18:29:53 -0300

    file and branch for MCMC traceplots generation@

--- inst/generating_traceplots.R
+++ inst/generating_traceplots.R
@@ -4,7 +4,7 @@ library(doParallel)
 library(subart)
 seed_ <- 43
 set.seed(seed_) # ORIGINal is 42
-n_ <- 250
+n_ <- 1000
 p_ <- 10
 n_tree_ <- 200
 n_mcmc_ <- 10000
@@ -43,13 +43,13 @@ if(task_ == "regression" & sim_ == "friedman1"){
 x_train <- train$x
 x_test <- train$x
 y_train <- train$y
-nu_prop <- nrow(x_train)*ncol(y_train)
+nu_prop <- 100
 # Doing the same for the MVN-BART
 subart_mod <- if(task_ == "regression"){
      subart::subart(x_train = x_train,y_mat = y_train,x_test = x_test,
                                   n_tree = n_tree_,n_mcmc = n_mcmc_,n_burn = n_burn_,nu = 2) # Maybe need to change the df to 10
 } else if(task_ == "classification"){
-     subart::subart(x_train = x_train,y_mat = y_train,x_test = x_test,m = ,
+     subart::subart(x_train = x_train,y_mat = y_train,x_test = x_test,m = nu_prop,
                                   n_tree = n_tree_,n_mcmc = n_mcmc_,n_burn = n_burn_,nu = 2)
 }
 
@@ -57,8 +57,20 @@ subart_mod <- if(task_ == "regression"){
 storing_posterior <- subart_mod$all_Sigma_post
 storing_ess <- subart_mod$ESS
 
+bart_mod <- dbarts::bart(x.train = x_train,y.train = y_train[,1],x.test = x_test,
+                         ntree = 200,ndpost = 10000,nskip = 0)
+
+# plot(bart_mod$yhat.train[2001:10000,], type = 'l',ylim = c(-2,2),ylab = expression(y[1,1]),
+#      xlab = "mcmc iter", col = ggplot2::alpha("black", 0.5))
+# lines(subart_mod$y_hat[1,1,], type = 'l',  col = ggplot2::alpha("blue", 0.5))
+#
+# ESS(bart_mod$yhat.train[2001:10000,1])
+# ESS(subart_mod$y_hat[1,1,])
+
 mcmc_obj <- list(all_Sigma_iter = storing_posterior,
-                 ess = storing_ess)
+                 ess = storing_ess,
+                 y_hat = subart_mod$y_hat,
+                 bart_mod = bart_mod$yhat.train)
 
 path <- "inst/mcmc_result/"
 saveRDS(mcmc_obj,file = paste0(path,"seed_",seed_,"_",model,"_",sim_,"_",task_,"_n_",n_,"_p_",p_,
